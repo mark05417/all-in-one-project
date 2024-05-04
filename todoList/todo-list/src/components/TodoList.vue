2ebe6a90ddd192c50ebc457c22ce49441735ad65
mark <mark05417@gmail.com>
2024-05-04 22:06:34 +0800

    TodoList.vue

---
+++ todoList/todo-list/src/components/TodoList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <input type="text" v-model="newTodo" @keyup.enter="addTodo">
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">{{ todo }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodo: '',
+      todos: []
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() !== '') {
+        this.todos.push(this.newTodo);
+        this.newTodo = '';
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* 可以添加一些 CSS 樣式來美化你的 TodoList */
+</style>
