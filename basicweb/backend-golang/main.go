c1bfb2089514397ac4f5446d4f05448f03064efa
mark <mark05417@gmail.com>
2024-10-21 18:47:32 +0800

    [golang] basic

---
+++ basicweb/backend-golang/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	response := Response{Message: "Hello, World!"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+
+	// 啟動伺服器並監聽在 :8080 端口
+	log.Println("Server is running on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
