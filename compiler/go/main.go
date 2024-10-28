53d3438bd075fb0e13e78fda8e245157cc232a1b
mark <mark05417@gmail.com>
2024-10-28 12:23:39 +0800

    [compiler] basic parser the generates parse tree

--- compiler/go/main.go
+++ compiler/go/main.go
@@ -259,39 +259,6 @@ func (s *Scanner) nextToken() Token {
 	}
 }
 
-// <program>       ::= <statements>
-// <statements>    ::= <statement> <statements>
-//                  | ε
-// <statement>     ::= <declaration>
-//                  | <comment>
-//                  | <if_statement>
-//                  | <while_loop>
-//                  | <print_statement>
-//                  | <increment>
-// <declaration>   ::= "int" <identifier> "=" <expression> ";"
-//                  | "bool" <identifier> "=" <boolean> ";"
-// <comment>       ::= "//" <comment_text>
-//                  | "/*" <comment_text> "*/"
-// <if_statement>  ::= "if" "(" <expression> ")" "{" <statements> "}"
-//                      "else" "{" <statements> "}"
-// <while_loop>    ::= "while" "(" <expression> ")" "{" <statements> "}"
-// <print_statement> ::= "print" <expression> ";"
-// <increment>     ::= <identifier> "++" ";"
-// <expression>    ::= <integer>
-//                  | <boolean>
-//                  | <identifier>
-//                  | <comparison>
-// <boolean>       ::= "true"
-//                  | "false"
-// <comparison>    ::= <expression> "<" <expression>
-//                  | <expression> ">" <expression>
-//                  | <expression> "<=" <expression>
-//                  | <expression> ">=" <expression>
-//                  | <expression> "==" <expression>
-//                  | <expression> "!=" <expression>
-// <identifier>    ::= [a-zA-Z_][a-zA-Z0-9_]*
-// <integer>       ::= [0-9]+
-
 // Parser Class
 type Parser struct {
 	tokens       []Token
@@ -311,25 +278,262 @@ func (p *Parser) PeekToken() Token {
 
 func (p *Parser) ConsumeToken(expectedToken Token) Token {
 	token := p.PeekToken()
-	if token.Type == expectedToken.Type && token.Value == expectedToken.Value {
+	if token.Type == expectedToken.Type { // && token.Value == expectedToken.Value {
 		p.currentIndex += 1
 		return token
 	} else {
-		os.Exit(-1)
+		os.Exit(66)
 		return Token{}
 	}
 }
 
+func (p *Parser) MatchToken(expectedToken Token) bool {
+	token := p.PeekToken()
+	if p.currentIndex >= len(p.tokens) {
+		return false
+	}
+	if token.Type == expectedToken.Type { //&& token.Value == expectedToken.Value {
+		return true
+	}
+	return false
+}
+
+type Node struct {
+	Type     string
+	Value    string
+	Children []Node
+}
+
+func NewNode(type_ string, value string) Node {
+	return Node{
+		Type:  type_,
+		Value: value,
+	}
+}
+
+func (n *Node) AddChild(child Node) {
+	n.Children = append(n.Children, child)
+}
+
+// <program>             ::= <statements>
+// <statements>          ::= <statement> <statements> | ε
+// <statement>           ::= <declaration>
+//                         | <comment>
+//                         | <if_statement>
+//                         | <while_loop>
+//                         | <print_statement>
+//                         | <incrementdecrement>
+// <declaration>         ::= "int" <identifier> "=" <expression> ";"
+//                         | "bool" <identifier> "=" <boolean> ";"
+// <comment>             ::= "//" <comment_text>
+//                         | "/*" <comment_text> "*/"
+// <if_statement>        ::= "if" "(" <expression> ")" <block>
+//                           "else" <block>
+// <while_loop>          ::= "while" "(" <expression> ")" <block>
+// <block>				 ::= "{" <statements> "}"
+// <print_statement>     ::= "print" <expression> ";"
+// <incrementdecrement>  ::= <identifier> "++" ";"
+//                         | <identifier> "--" ";"
+// <expression>          ::= <integer> (">=" | ">" | "<=" | "<" | "==" | "!=" | "+" | "-" | "*" | "/" | "%" <expression>)*
+//                         | <boolean> (">=" | ">" | "<=" | "<" | "==" | "!=" | "+" | "-" | "*" | "/" | "%" <expression>)*
+//                         | <identifier> (">=" | ">" | "<=" | "<" | "==" | "!=" | "+" | "-" | "*" | "/" | "%" <expression>)*
+// <boolean>             ::= "true"
+//                         | "false"
+// <identifier>          ::= [a-zA-Z_][a-zA-Z0-9_]*
+// <integer>             ::= [0-9]+
+
+func (p *Parser) Parse() Node {
+	return p.ParseProgram()
+}
+
+func (p *Parser) ParseProgram() Node {
+	root := NewNode("Program", "Program")
+
+	statements := p.ParseStatements()
+	root.AddChild(statements)
+
+	return root
+}
+
+func (p *Parser) ParseStatements() Node {
+	root := NewNode("Statements", "Statements")
+	for p.MatchToken(Token{Type: "eof", Value: "eof"}) == false {
+		statement := p.ParseStatement()
+		root.AddChild(statement)
+	}
+	return root
+}
+
+func (p *Parser) ParseStatement() Node {
+	root := NewNode("Statement", "Statement")
+	if p.MatchToken(Token{Type: "if", Value: "if"}) {
+		root.AddChild(p.ParseIfStatement())
+	} else if p.MatchToken(Token{Type: "while", Value: "while"}) {
+		root.AddChild(p.ParseWhileLoop())
+	} else if p.MatchToken(Token{Type: "print", Value: "print"}) {
+		root.AddChild(p.ParsePrintStatement())
+	} else if p.MatchToken(Token{Type: "int", Value: "int"}) {
+		root.AddChild(p.ParseDeclaration())
+	} else if p.MatchToken(Token{Type: "bool", Value: "bool"}) {
+		root.AddChild(p.ParseDeclaration())
+	} else if p.MatchToken(Token{Type: "linecomment", Value: "linecomment"}) {
+		root.AddChild(p.ParseLineComment())
+	} else if p.MatchToken(Token{Type: "blockcomment", Value: "blockcomment"}) {
+		root.AddChild(p.ParseBlockComment())
+	} else if p.MatchToken(Token{Type: "identifier", Value: "identifier"}) {
+		root.AddChild(p.ParseIncrementDecrement())
+	} else {
+		os.Exit(77)
+		return Node{}
+	}
+	return root
+}
+func (p *Parser) ParseIfStatement() Node {
+	root := NewNode("IfStatement", "IfStatement")
+	p.ConsumeToken(Token{Type: "if", Value: "if"})
+	p.ConsumeToken(Token{Type: "left_paren", Value: "("})
+	root.AddChild(p.ParseExpression())
+	p.ConsumeToken(Token{Type: "right_paren", Value: ")"})
+	root.AddChild(p.ParseBlock())
+
+	if p.MatchToken(Token{Type: "else", Value: "else"}) {
+		p.ConsumeToken(Token{Type: "else", Value: "else"})
+		root.AddChild(NewNode("ElseStatement", "ElseStatement"))
+		root.AddChild(p.ParseBlock())
+	}
+	return root
+}
+
+func (p *Parser) ParseWhileLoop() Node {
+	root := NewNode("WhileLoop", "WhileLoop")
+	p.ConsumeToken(Token{Type: "while", Value: "while"})
+	p.ConsumeToken(Token{Type: "left_paren", Value: "("})
+	root.AddChild(p.ParseExpression())
+	p.ConsumeToken(Token{Type: "right_paren", Value: ")"})
+	root.AddChild(p.ParseBlock())
+	return root
+}
+
+func (p *Parser) ParseBlock() Node {
+	root := NewNode("Block", "Block")
+	p.ConsumeToken(Token{Type: "left_brace", Value: "{"})
+	for !p.MatchToken(Token{Type: "right_brace", Value: "}"}) {
+		root.AddChild(p.ParseStatement())
+	}
+	p.ConsumeToken(Token{Type: "right_brace", Value: "}"})
+	return root
+}
+
+func (p *Parser) ParsePrintStatement() Node {
+	root := NewNode("PrintStatement", "PrintStatement")
+	p.ConsumeToken(Token{Type: "print", Value: "print"})
+	root.AddChild(p.ParseExpression())
+	p.ConsumeToken(Token{Type: "semicolon", Value: ";"})
+	return root
+}
+
+func (p *Parser) ParseDeclaration() Node {
+	root := NewNode("Declaration", "Declaration")
+	if p.MatchToken(Token{Type: "int", Value: "int"}) {
+		root.AddChild(NewNode("Type", "int"))
+		p.ConsumeToken(Token{Type: "int", Value: "int"})
+	} else if p.MatchToken(Token{Type: "bool", Value: "bool"}) {
+		root.AddChild(NewNode("Type", "bool"))
+		p.ConsumeToken(Token{Type: "bool", Value: "bool"})
+	}
+
+	if !p.MatchToken(Token{Type: "identifier", Value: "identifier"}) {
+		os.Exit(88)
+	}
+	root.AddChild(NewNode("Identifier", p.PeekToken().Value))
+	p.ConsumeToken(Token{Type: "identifier", Value: "identifier"})
+
+	if !p.MatchToken(Token{Type: "assign", Value: "assign"}) {
+		os.Exit(99)
+	}
+	root.AddChild(NewNode("Assign", "assign"))
+	p.ConsumeToken(Token{Type: "assign", Value: "assign"})
+
+	root.AddChild(p.ParseExpression())
+
+	p.ConsumeToken(Token{Type: "semicolon", Value: ";"})
+	return root
+}
+
+func (p *Parser) ParseLineComment() Node {
+	root := NewNode("LineComment", "LineComment")
+	root.AddChild(NewNode("linecomment", p.PeekToken().Value))
+	p.ConsumeToken(Token{Type: "linecomment", Value: "linecomment"})
+	return root
+}
+
+func (p *Parser) ParseBlockComment() Node {
+	root := NewNode("BlockComment", "BlockComment")
+	root.AddChild(NewNode("blockcomment", p.PeekToken().Value))
+	p.ConsumeToken(Token{Type: "blockcomment", Value: "blockcomment"})
+	return root
+}
+
+func (p *Parser) ParseIncrementDecrement() Node {
+	root := NewNode("IncrementDecrement", "IncrementDecrement")
+	root.AddChild(NewNode("identifier", p.PeekToken().Value))
+	p.ConsumeToken(Token{Type: "identifier", Value: "identifier"})
+	if p.MatchToken(Token{Type: "increment", Value: "increment"}) {
+		root.AddChild(NewNode("increment", "increment"))
+		p.ConsumeToken(Token{Type: "increment", Value: "increment"})
+	} else if p.MatchToken(Token{Type: "decrement", Value: "decrement"}) {
+		root.AddChild(NewNode("decrement", "decrement"))
+		p.ConsumeToken(Token{Type: "decrement", Value: "decrement"})
+	}
+	p.ConsumeToken(Token{Type: "semicolon", Value: ";"})
+	return root
+}
+
+func (p *Parser) ParseExpression() Node {
+	root := NewNode("Expression", "Expression")
+
+	if p.MatchToken(Token{Type: "integer", Value: "integer"}) {
+		root.AddChild(NewNode("integer", p.PeekToken().Value))
+		p.ConsumeToken(Token{Type: "integer", Value: "integer"})
+	} else if p.MatchToken(Token{Type: "identifier", Value: "identifier"}) {
+		root.AddChild(NewNode("Identifier", p.PeekToken().Value))
+		p.ConsumeToken(Token{Type: "identifier", Value: "identifier"})
+	} else if p.MatchToken(Token{Type: "true", Value: "true"}) {
+		root.AddChild(NewNode("True", p.PeekToken().Value))
+		p.ConsumeToken(Token{Type: "true", Value: "true"})
+	} else if p.MatchToken(Token{Type: "false", Value: "false"}) {
+		root.AddChild(NewNode("False", p.PeekToken().Value))
+	}
+
+	if p.MatchToken(Token{Type: "less_equal", Value: "<="}) ||
+		p.MatchToken(Token{Type: "less", Value: "<"}) ||
+		p.MatchToken(Token{Type: "greater_equal", Value: ">="}) ||
+		p.MatchToken(Token{Type: "greater", Value: ">"}) ||
+		p.MatchToken(Token{Type: "equal", Value: "=="}) ||
+		p.MatchToken(Token{Type: "not_equal", Value: "!="}) {
+
+		root.AddChild(NewNode("Operator", p.PeekToken().Value))
+		p.ConsumeToken(Token{Type: p.PeekToken().Type, Value: p.PeekToken().Value})
+		root.AddChild(p.ParseExpression())
+	}
+	return root
+}
 func main() {
-	fmt.Println(dataString)
+	fmt.Println(dataString + "\n====")
 
 	reader := NewReader(dataString)
 	scanner := NewScanner(reader)
+	Tokens := make([]Token, 0)
 	for {
 		token := scanner.nextToken()
 		fmt.Println(token)
+		Tokens = append(Tokens, token)
 		if token.Type == "eof" {
 			break
 		}
 	}
+
+	parser := NewParser(Tokens)
+	root := parser.Parse()
+	fmt.Println("====\n", root)
 }
